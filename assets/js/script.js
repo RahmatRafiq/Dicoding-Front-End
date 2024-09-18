6da07008e5c02988819432313dfd3107fd2b416c
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-18 22:18:37 +0800

    Add sticky header and toggle functionality for navbar menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 const header = document.querySelector("header");
 
+// Menambahkan class 'sticky' saat scroll
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// Toggle navbar saat menu icon diklik
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
+  menu.classList.toggle("bx-x"); // Ubah ikon menjadi X (menu close)
+  navbar.classList.toggle("active"); // Toggle tampilan navbar
 };
